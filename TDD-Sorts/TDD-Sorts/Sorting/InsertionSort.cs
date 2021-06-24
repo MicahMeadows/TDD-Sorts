aac24277965cd9d50b63aae31b664223052f95cb
MicahMeadows <micahstlp@gmail.com>
2021-06-24 16:05:59 -0400

    Add failing unit test for insertion sort

---
+++ TDD-Sorts/TDD-Sorts/Sorting/InsertionSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Sorts.Sorting
+{
+    public class InsertionSort
+    {
+        public static void Sort(int[] numbers)
+        {
+            
+        }
+    }
+}
---
+++ TDD-Sorts/TDD-SortsTests/InsertionSortTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using TDD_Sorts.Sorting;
+
+namespace TDD_SortsTests
+{
+    [TestFixture]
+    class InsertionSortTests
+    {
+        [Test]
+        [TestCase(new int[]{4,5,3,1,2}, new int[]{1,2,3,4,5})]
+        public void InsertionSort_WhenUnsortedPositiveArray_ReturnsSortedPositiveArray(int[] unsortedInput, int[] sortedOutput)
+        {
+            // Act
+            InsertionSort.Sort(unsortedInput);
+
+            // Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(unsortedInput, sortedOutput));
+        }
+    }
+}
