cb3f937aa5a9784bbf0f1cfe6092de64804258a4
MicahMeadows <micahstlp@gmail.com>
2021-06-24 15:34:36 -0400

    Add simple bubble sort to make test pass

--- TDD-Sorts/TDD-Sorts/Sorting/BubbleSort.cs
+++ TDD-Sorts/TDD-Sorts/Sorting/BubbleSort.cs
@@ -4,9 +4,20 @@ namespace TDD_Sorts.Sorting
 {
     public class BubbleSort
     {
-        public static void Sort(int[] unsortedInput)
+        public static void Sort(int[] input)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input.Length - 1; j++)
+                {
+                    if (input[j] > input[j + 1])
+                    {
+                        int t = input[j];
+                        input[j] = input[j + 1];
+                        input[j + 1] = t;
+                    }
+                }
+            }
         }
     }
 }
