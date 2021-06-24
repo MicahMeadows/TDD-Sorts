2e5dcd175b575f6ceaf23bff8784ee722ba61680
MicahMeadows <micahstlp@gmail.com>
2021-06-24 15:35:10 -0400

    Add another simple longer sort test

--- TDD-Sorts/TDD-SortsTests/BubbleSortTests.cs
+++ TDD-Sorts/TDD-SortsTests/BubbleSortTests.cs
@@ -21,5 +21,18 @@ namespace TDD_Sorts.Tests
             int[] sortedOutput = { 2, 5, 6, 7, 8 };
             Assert.IsTrue(Enumerable.SequenceEqual(unsortedInput, sortedOutput));
         }
+        [Test]
+        public void BubbleSort_WhenFourNineSevenSixThreeNineOne_ReturnsWhenOneThreeFourSixSevenNineNine()
+        {
+            // Arrange
+            int[] unsortedInput = { 4, 9, 7, 6, 3, 9, 1 };
+
+            // Act
+            BubbleSort.Sort(unsortedInput);
+
+            // Assert
+            int[] sortedOutput = { 1, 3, 4, 6, 7, 9, 9 };
+            Assert.IsTrue(Enumerable.SequenceEqual(unsortedInput, sortedOutput));
+        }
     }
 }
