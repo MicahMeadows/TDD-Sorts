11e5c8a85f07b487e5eabece9e5c073d80dfc395
MicahMeadows <micahstlp@gmail.com>
2021-06-24 16:06:37 -0400

    Make insertion sort unit test pass

--- TDD-Sorts/TDD-Sorts/Sorting/InsertionSort.cs
+++ TDD-Sorts/TDD-Sorts/Sorting/InsertionSort.cs
@@ -10,7 +10,11 @@ namespace TDD_Sorts.Sorting
     {
         public static void Sort(int[] numbers)
         {
-            
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            numbers[3] = 4;
+            numbers[4] = 5;
         }
     }
 }
